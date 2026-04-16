5dbeb5a35cace2e3f9a8b208a3781bdca4d08f03
denizoezerol-droid <denizoezerol@gmail.com>
2026-04-16 21:46:26 +0200

    Add default salon and customer data

--- src/demoData.js
+++ src/demoData.js
@@ -1,1 +1,25 @@
+export const DEFAULT_SALON = {
+  name: "Haarwerk Studio",
+  primaryColor: "#C8A96A",
+};
 
+export const CUSTOMERS = [
+  {
+    id: "c1",
+    name: "Laura Becker",
+    email: "laura.becker@email.de",
+
+    history: [
+      {
+        date: "01.03.2026",
+        service: "Balayage + Schnitt",
+        note: "Kundin möchte beim nächsten Mal kühleres Blond"
+      },
+      {
+        date: "15.01.2026",
+        service: "Waschen + Schneiden",
+        note: "Sehr empfindliche Kopfhaut"
+      }
+    ]
+  }
+];

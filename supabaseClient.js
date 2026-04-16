55db9157f4b85f192ed32d9841d17352f1bb6d7f
denizoezerol-droid <denizoezerol@gmail.com>
2026-04-16 21:56:39 +0200

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://kcqtuahlfpphzarxphkp.supabase.co",
-  "sb_publishable_N0FER2btsCt63RJgE0F43A_lR_Q9jnW"
-);
